11f55b4f9d939a70b4b70ab584b0797a76d52175
Priyanshu-Patil <priyanshpatil1202@gmail.com>
2025-08-13 15:25:15 +0530

    Add Snackbar context and component; implement linear progress indicator in Register page

--- src/components/progress.jsx
+++ src/components/progress.jsx
@@ -1,14 +1,74 @@
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
+import { motion } from 'motion/react';
 
-const circularProgress = ({ classes = '', size = ''}) => {
-    return (
-        <div role="progressbar" className={`circular-progress ${size} ${classes}`}></div>
-    )
-}
+const CircularProgress = ({ classes = '', size = '' }) => {
+  return (
+    <div
+      role='progressbar'
+      className={`circular-progress ${size} ${classes}`}
+    ></div>
+  );
+};
 
-circularProgress.propTypes = {
-    classes: PropTypes.string,
-    size: PropTypes.string,
-}
+CircularProgress.propTypes = {
+  classes: PropTypes.string,
+  size: PropTypes.string,
+};
 
-export {circularProgress};
+const LinearProgress = ({ classes = '' }) => {
+  const progressVariants = {
+    start: { scaleY: 0 },
+    end: {
+      scaleY: 1,
+      transition: {
+        when: 'beforeChildren',
+        duration: 0.3,
+        ease: [0.05, 0.7, 0.1, 1],
+        delay: 0.5,
+      },
+    },
+    exit: {
+      scaleY: 0,
+      transition: { duration: 0.3, ease: [0.05, 0.7, 0.1, 1] },
+    },
+  };
+
+  const activeIndicatorVariants = {
+    start: {translateX: '-100%' },
+    end: {
+      translateX: '0%',
+      transition: {
+        duration: 0.3,
+        ease: [0.05, 0.7, 0.1, 1],
+        delay: 0.5,
+        },
+    },
+  };
+
+  return (
+    <motion.div
+      variants={progressVariants}
+      initial='start'
+      animate='end'
+      exit='exit'
+      role='progressbar'
+      className={`linear-progress ${classes}`}
+    >
+      <motion.div
+        variants={activeIndicatorVariants}
+        transition={{
+            repeat: Infinity,
+            duration: 1.5,
+            ease: [0.05, 0.7, 0.1, 1],
+        }}
+        className='active-indicator'
+      ></motion.div>
+    </motion.div>
+  );
+};
+
+LinearProgress.propTypes = {
+  classes: PropTypes.string,
+};
+
+export { CircularProgress, LinearProgress };
